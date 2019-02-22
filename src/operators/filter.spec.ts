7a66e357191f359cf195a68febd471e6f3a714eb
Kwinten Pisman <kwinten.pisman@gmail.com>
2019-02-22 13:29:09 +0100

    filter operator

---
+++ src/operators/filter.spec.ts
@@ -0,0 +1,15 @@
+import { of } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { myFilter } from './filter';
+
+test('create our own filter operator', (done) => {
+    let expectedResult = [2, 4];
+
+    of(1, 2, 3, 4, 5).pipe(
+        myFilter((x: number) => x % 2 === 0)
+    ).subscribe(val => {
+        const expected = expectedResult.shift();
+        expect(val).toBe(expected);
+        done();
+    });
+});
---
+++ src/operators/filter.ts
@@ -0,0 +1,16 @@
+// operator -> function that takes an Observable and returns an Observable
+
+import { Observable, Observer } from 'rxjs';
+
+export const myFilter = <T> (filter: (n: T) => boolean) => (source: Observable<T>) => {
+    return new Observable((observer: Observer<T>) => {
+        const subscription = source.subscribe(
+            (next: T) => {
+                if(filter(next)) {
+                    observer.next(next);
+                }
+            }
+        );
+        return subscription;
+    });
+};
