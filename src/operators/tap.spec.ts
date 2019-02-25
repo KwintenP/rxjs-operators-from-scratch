1bbada811227ff80c6912551b81de77d7f7f95a2
Kwinten Pisman <kwinten.pisman@gmail.com>
2019-02-25 17:49:58 +0100

    add tap operator

--- src/operators/tap.spec.ts
+++ src/operators/tap.spec.ts
@@ -1,17 +1,18 @@
 import { of } from 'rxjs';
-import { myMap } from './map';
-import { map, tap } from 'rxjs/operators';
+import { tap } from 'rxjs/operators';
+import { myTap } from './tap';
 
 test('create our own map operator', (done) => {
     let expectedResult = [1, 2, 3, 4, 5];
 
     of(1, 2, 3, 4, 5).pipe(
-        tap(val => {
-            const expected = expectedResult.shift();
-            expect(val).toBe(expected);
-            if (expectedResult.length === 0) {
-                done();
+        myTap(val => {
+                const expected = expectedResult.shift();
+                expect(val).toBe(expected);
+                if (expectedResult.length === 0) {
+                    done();
+                }
             }
-        })
+        )
     ).subscribe();
 });
--- src/operators/tap.ts
+++ src/operators/tap.ts
@@ -1,13 +1,25 @@
 import { Observable, Observer } from 'rxjs';
 
-export const myMap = <T, E>(map: (n: T) => E) => (source: Observable<T>) => {
-    return new Observable((observer: Observer<E>) => {
-        const subscription = source.subscribe(
-            (next: T) => map(next),
-            (err: any) => observer.error(err),
-            () => observer.complete(),
-        );
+export const myTap = <T>(nextFn: (n: T) => void,
+                         errorFn?: (err: any) => void,
+                         completeFn?: () => void) =>
+    (source: Observable<T>) => {
+        return new Observable((observer: Observer<E>) => {
+            const subscription = source.subscribe(
+                (next: T) => {
+                    nextFn(next);
+                    observer.next(next);
+                },
+                (err: any) => {
+                    errorFn && errorFn(err);
+                    observer.error(err);
+                },
+                () => {
+                    completeFn && completeFn();
+                    observer.complete();
+                }
+            );
 
-        return subscription;
-    });
-};
+            return subscription;
+        });
+    };
